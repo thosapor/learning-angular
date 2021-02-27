551584773fc7db7eec8faa51000bbbb5389bd71d
thosapor <thosapor@hotmail.com>
2021-02-27 21:12:07 +0700

    delete other ex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HomeComponent} from './home/home.component'
 import {FormsModule} from '@angular/forms'
-import { CountingComponent } from './counting/counting.component';
 import { Lab4Component } from './lab4/lab4.component';
 
 
@@ -12,8 +10,6 @@ import { Lab4Component } from './lab4/lab4.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CountingComponent,
     Lab4Component,
   ],
   imports: [
