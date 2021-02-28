a6e6677c82057dfece21684b00c98aaacead5eed
thosapor <thosapor@hotmail.com>
2021-02-28 09:49:35 +0700

    push to ex13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HomeComponent} from './home/home.component'
-import {FormsModule} from '@angular/forms'
-import { CountingComponent } from './counting/counting.component';
+import { HomeComponent } from './home/home.component'
+import { FormsModule } from '@angular/forms'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CountingComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
